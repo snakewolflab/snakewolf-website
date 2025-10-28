6dc73b52667d7674662c0729d01459dbb7190173
pay school <schoolpay2024@gmail.com>
2025-10-28 13:37:47 +0000

    2025-10-28T13:34:32.0191725Z ##[group]Run npm run build npm run build sh

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  distDir: 'out',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -27,7 +25,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' as const,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
